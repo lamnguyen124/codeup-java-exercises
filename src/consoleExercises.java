ddd95f491468904d0a7cd26a69e66de338fad46b
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-25 14:53:31 -0500

    consoleExercise

--- src/consoleExercises.java
+++ src/consoleExercises.java
@@ -1,22 +1,41 @@
 import java.util.Scanner;
+
 public class consoleExercises {
     public static void main(String[] args) {
-        double pi = 3.14159;
-        System.out.format("The value of pi is approximately %s", pi);
+        double pi = 3.141592;
+        System.out.printf("The value of pi is %napproximately %.2f.%n", pi);
 
         Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        System.out.print("Please enter an integer (make sure you do it right): ");
+        int input = scanner.nextInt();
+        System.out.println(input);
 
-        System.out.print("Enter something: ");
-        String userInput = scanner.nextLine();
+        System.out.print("Please enter three words: ");
+        String input1 = scanner.next();
+        String input2 = scanner.next();
+        String input3 = scanner.next();
+        System.out.println(input1);
+        System.out.println(input2);
+        System.out.println(input3);
+        //nextLine() called to clear input
+        scanner.nextLine();
+        System.out.printf("%nPlease enter a sentence: ");
+        String sentence = scanner.nextLine();
+        System.out.printf("Here is your sentence: %s%n", sentence);
 
-        System.out.println("You entered: --> \"" + userInput + "\" <--");
+        System.out.printf("What is the width of the classroom?: ");
+        String answer = scanner.nextLine();
+        double width = Double.valueOf(answer);
+//        double width = new Double(answer);
+        System.out.println(width);
+        System.out.printf("What is the length of the classroom?: ");
+        answer = scanner.nextLine();
+        double length = Double.valueOf(answer);
 
+        System.out.printf("Area of classroom = %.2f%n", width * length);
+        System.out.printf("Perimeter of classroom = %.2f%n", 2 * width + 2 * length);
+        System.out.printf("Volume of classroom = %.2f%n", width * length * 10.0);
 
-        Scanner scanner2 = new Scanner(System.in);
-
-        System.out.print("What is the length and width of the classroom?: ");
-        String userInput3 = scanner2.nextLine();
-
-//        System.out.println("The area is:" +);
     }
 }
