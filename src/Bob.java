8b74d33409313b6f03f3b576877de900abfc2a10
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-26 16:13:29 -0500

    update

--- src/Bob.java
+++ src/Bob.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Say something: ");
+        String input = scanner.next();
+        if (input.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (input.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (input.equals(" ")) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever.");
+        }
+
+    }
 }
+
