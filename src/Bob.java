4e71c19a1ba5095adf3599134727bcf81d41ec52
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-29 09:57:59 -0500

    update

--- src/Bob.java
+++ src/Bob.java
@@ -1,18 +1,53 @@
+//import java.util.Scanner;
+//public class Bob {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Say something: ");
+//        String input = scanner.nextLine();
+//        if (input.endsWith("?")) {
+//            System.out.println("Sure");
+//        } else if (input.endsWith("!")) {
+//            System.out.println("Whoa, chill out!");
+//        } else if (input.isEmpty()) {
+//            System.out.println("Fine. Be that way!");
+//        } else {
+//            System.out.println("Whatever.");
+//        }
+//
+//    }
+//}
+
 import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Say something: ");
-        String input = scanner.nextLine();
-        if (input.endsWith("?")) {
-            System.out.println("Sure");
-        } else if (input.endsWith("!")) {
-            System.out.println("Whoa, chill out!");
-        } else if (input.isEmpty()) {
-            System.out.println("Fine. Be that way!");
-        } else {
-            System.out.println("Whatever.");
-        }
+
+        boolean talking = true;
+        String message = "";
+        System.out.println("Bob is eating cereal...");
+
+        do {
+            message = scanner.nextLine().toLowerCase();
+
+            if (message.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (message.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (message.isEmpty()) {
+                System.out.println("Fine. Be that way!");
+//            } else if (message.equalsIgnoreCase("bye") ||
+//                        message.equalsIgnoreCase("bye."))
+            } else if (message.contains("bye")) {
+                talking = false;
+            } else {
+                System.out.println("Whatever.");
+            }
+        } while (talking);
+
+        System.out.println("Bob continues eating cereal...");
 
     }
 }
