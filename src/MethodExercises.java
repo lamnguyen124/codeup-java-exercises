8a9c73cb87ddcbfdf95ff436e1a04aa32af56df6
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-29 17:00:27 -0500

    update

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -2,57 +2,35 @@ public class MethodExercises {
     public static double add(double a, double b) {
         return a + b;
     }
-    public static double substract(double a, double b) {
+    public static double subtract(double a, double b) {
         return a - b;
     }
-    public static double multiply(double a, double b) {
-        return a * b;
+    public static double multiply(double a, int b) {
+//        return a * b;
+        //using loop for repeated addition
+//        double result = 0;
+//        for (int i = 0; i < b; i++){
+//            result += a;
+//        }
+//        return result;
+
+        //using recursion for repeated addition
+        if (b <= 0) return 0;
+        return a + multiply(a, b-1);
     }
     public static double divide(double a, double b) {
         return a/b;
     }
-
-
+    public static double modulus(double a, double b) {
+        return a % b;
+    }
     public static void main(String[] args) {
-    System.out.println(add(3,17));
-    System.out.println((substract(3,4)));
-    System.out.println(multiply(3,17));
-    System.out.println(divide(3,17));
+    System.out.println(add(1,9));
+    System.out.println((subtract(3,4)));
+    System.out.println(multiply(6,12));
+    System.out.println(divide(5,20));
+    System.out.println(modulus(8,22));
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-//        int num = 6;
-//        int num2 = 4;
-//
-//        adding();
-//        substracting();
-//        dividing();
-//        multiplying();
-//    }
-//}
-// public static void adding(int num, int num2) {
-//    return (num + num2);
-// }
-//}
-//
-//public static void substracting(int num, int num2) {
-//        return (num - num2);
-//        }
-//        }
-//
-//public static void divding(int num, int num2) {
-//        return (num / num2);
-//
-//public static void multiplying(int num, int num2) {
-//        return (num * num2);
