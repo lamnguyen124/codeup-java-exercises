cdffd66943e4147fde46703b6ff44c3061b12179
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-29 16:39:53 -0500

    methods

---
+++ src/MethodExercises.java
@@ -0,0 +1,58 @@
+public class MethodExercises {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static double substract(double a, double b) {
+        return a - b;
+    }
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+    public static double divide(double a, double b) {
+        return a/b;
+    }
+
+
+    public static void main(String[] args) {
+    System.out.println(add(3,17));
+    System.out.println((substract(3,4)));
+    System.out.println(multiply(3,17));
+    System.out.println(divide(3,17));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//        int num = 6;
+//        int num2 = 4;
+//
+//        adding();
+//        substracting();
+//        dividing();
+//        multiplying();
+//    }
+//}
+// public static void adding(int num, int num2) {
+//    return (num + num2);
+// }
+//}
+//
+//public static void substracting(int num, int num2) {
+//        return (num - num2);
+//        }
+//        }
+//
+//public static void divding(int num, int num2) {
+//        return (num / num2);
+//
+//public static void multiplying(int num, int num2) {
+//        return (num * num2);
