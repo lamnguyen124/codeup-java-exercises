c6c24252e499461e662e795966d3963768493ee0
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-08-01 10:16:43 -0500

    update

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,13 +2,31 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
+    private static Person[] addPerson(Person[] group, Person newMember) {
+        Person[] newGroup = Arrays.copyOf(group, group.length + 1);
+        newGroup[newGroup.length-1] = newMember;
+        return newGroup;
+    }
+
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
+
         Person[] people = new Person[3];
-        Person[] people = {new Person("Ally"), new Person("Happy"), new Person("Nala")}
-        for (int i = 0; i < Person.length; i += 1) {
-            System.out.println(Person[i]);
+        people[0] = new Person("Larry");
+        people[1] = new Person("Moe");
+        people[2] = new Person("Carl");
+//        Person[] people = {new Person("Larry"), new Person("Moe"), new Person("Curly")};
+
+//        for (Person character : people) {
+//            System.out.println(character.getName());
+//        }
+
+        Person[] newGroup = addPerson(people, new Person("Linda"));
+//        Arrays.toString(newGroup);
+        for (Person character : newGroup) {
+            System.out.println(character.getName());
         }
+
     }
 }
--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,32 +1,11 @@
-import java.util.Arrays;
-
-public class ArraysExercises {
-
-    private static Person[] addPerson(Person[] group, Person newMember) {
-        Person[] newGroup = Arrays.copyOf(group, group.length + 1);
-        newGroup[newGroup.length-1] = newMember;
-        return newGroup;
-    }
-
+import java.util.Random;
+public class ServerNameGenerator {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(numbers));
-
-        Person[] people = new Person[3];
-        people[0] = new Person("Larry");
-        people[1] = new Person("Moe");
-        people[2] = new Person("Carl");
-//        Person[] people = {new Person("Larry"), new Person("Moe"), new Person("Curly")};
-
-//        for (Person character : people) {
-//            System.out.println(character.getName());
-//        }
-
-        Person[] newGroup = addPerson(people, new Person("Linda"));
-//        Arrays.toString(newGroup);
-        for (Person character : newGroup) {
-            System.out.println(character.getName());
-        }
-
+        String[] Adjectives = {"attractive", "beautiful", "dazzling", "elegant", "gorgeous", "shapely", "fit", "muscular", "fancy", "skinny"};
+        String[] Noun = {"cat", "dog", "turtle", "elephant", "dolphin", "parrot", "fish", "shark", "tiger", "lion"};
+        Random random = new Random();
+        int randomadj = (random.nextInt(Adjectives.length));
+        int randomnoun = (random.nextInt(Noun.length));
+        System.out.println((Adjectives[randomadj]) + " " + (Noun[randomnoun]));
     }
 }
