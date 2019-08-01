1dbf871362fc5b7dd22ae5e3befe5fe0aa20fa8e
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-08-01 10:14:17 -0500

    update

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
 
     public static void main(String[] args) {
-        String[] Person = {"Ally", "Happy", "Nala"};
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+        Person[] people = new Person[3];
+        Person[] people = {new Person("Ally"), new Person("Happy"), new Person("Nala")}
         for (int i = 0; i < Person.length; i += 1) {
             System.out.println(Person[i]);
         }
