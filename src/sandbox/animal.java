fb4c5d5b7713a217549ce13509ea894a42e56d56
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-08-01 16:01:57 -0500

    shapes

---
+++ src/sandbox/animal.java
@@ -0,0 +1,39 @@
+package sandbox;
+
+public class animal {
+
+    private String name;
+    private int brain;
+    private int body;
+    private int weight;
+
+    public animal(String name, int brain, int body, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.weight = weight;
+    }
+    public void eat(){
+
+    }
+
+    public void move() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
---
+++ src/sandbox/dog.java
@@ -0,0 +1,31 @@
+package sandbox;
+
+public class dog extends animal {
+
+    private int eyes;
+    private int legs;
+    private int tails;
+    private int teeth;
+    private String coat;
+
+    public dog(String name, int size, int, weight, int eyes, int legs, int tail, int teeth, String coat, int brain, int body, int weight) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+
+    }
+
+    private void chew() {
+        System.out.println("Dog.chew() called");
+    }
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        chew();
+        super.eat();
+    }
+}
+
