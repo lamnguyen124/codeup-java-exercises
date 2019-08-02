accbb82be77ac5021fdf2d1077bdbb08105384ec
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-08-02 16:33:26 -0500

    update

--- src/sandbox/Main.java
+++ src/sandbox/Main.java
@@ -1,7 +1,7 @@
-package sandbox;
-
-public class Main {
-    animal animal = new animal("Animal", 1, 1, 5);
-
-    dog dog = new dog("Yorkie", 8, 20, 2, 4, 20, "long silky", 1, "short", 20,);
-}
+//package sandbox;
+//
+//public class Main {
+//    animal animal = new animal("Animal", 1, 1, 5);
+//
+//    dog dog = new dog("Yorkie", 8, 20, 2, 4, 20, "long silky", 1, "short", 20,);
+//}
