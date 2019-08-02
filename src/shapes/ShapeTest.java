6d4a61b2253ca0df023edfe03e4898b1c8e98edf
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-08-02 09:17:10 -0500

    update

--- src/shapes/ShapeTest.java
+++ src/shapes/ShapeTest.java
@@ -4,13 +4,13 @@ public class ShapeTest {
 
     public static void main(String[] args) {
 
-        Rectangle box1 = new Rectangle(0, 5);
-        System.out.println("getArea = " + box1.getArea());
-        System.out.println("getPerimeter = " + box1.getPerimeter());
+        Rectangle box1 = new Rectangle(4, 5);
+        System.out.println(box1.getArea());
+        System.out.println(box1.getPerimeter());
 
         Rectangle box2 = new Square(5);
-        System.out.println("getArea = " + box2.getArea());
-        System.out.println("getPerimeter = " + box2.getPerimeter());
+        System.out.println(box2.getArea());
+        System.out.println(box2.getPerimeter());
 
     }
 
